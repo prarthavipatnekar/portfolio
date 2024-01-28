dd61f24be4e23e6637f5a5470e78d452ac8ae422
Ishika <Ishika.patnekar@gmail.com>
2024-01-28 14:45:36 -0700

    working on home and about me page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './About.module.css';
+import Me from './../../assets/myProfile.png'
+import PageHeader from './../PageHeader/PageHeader'
+
+
+const About = () => {
+    return (
+        <div className={classes.AboutMe}>
+            
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Hello, I'm Prarthavi!</h2> <br />
+                        I am software developer based in South Dakota, USA. My hometown in
+                        located in Goa, India.
+                   
+                </div>
+                <div className={classes.Photo}>
+                    <img className={classes.Me} src={Me} alt="Prarthavi Patnekar"></img>
+                </div>
+            </div>
+       </div>
+    );
+  };
+  
+export default About;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import classes from './Home.module.css';
+import Me from './../../assets/myProfile.png'
+import Navbar from './../Navbar/Navbar'
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+
+const Home = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = ["I'm Prarthavi Patnekar", "a software developer"];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
+    return (
+        <section className={classes.Home}>
+            <Container className={classes.Container}>
+                <Row >
+                    <Col xs={12} xl={12}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                                </div>}
+                        </TrackVisibility>
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>
+    )
+}
+export default Home;
+
